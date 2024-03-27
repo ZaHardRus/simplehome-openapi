d44512b84449f21ed017c2467173fd62fec1312d
Machalkas <al1999dk@gmail.com>
2024-03-27 10:33:40 +0300

    put back all components

--- pars_script/__init__.py
+++ pars_script/__init__.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Any, Optional
 
 from pars_script.exceptions import ParserException
 
@@ -7,26 +7,32 @@ from pars_script.exceptions import ParserException
 class Parser(ABC):
 
     parser_arguments_map = {
-        "--file": {"name": "file_path", "type": str},
-        "--format": {"name": "file_format", "type": str}
+        "--input": {"name": "input_file_path", "type": str},
+        "--format": {"name": "file_format", "type": str},
+        "--output": {"name": "output_file_path", "type": str}
     }
     file_format: str
 
     @classmethod
     @abstractmethod
-    def parse(cls, file_path: str):
+    def parse(cls, input_file_path: str):
         ...
 
     @staticmethod
     @abstractmethod
-    def read_file(file_path: str):
+    def read_file(input_file_path: str):
+        ...
+
+    @staticmethod
+    @abstractmethod
+    def write_file(input_file_path: str, data: Any):
         ...
 
     @classmethod
-    def run_parser(cls, file_path: str, file_format: Optional[str] = None):
+    def run_parser(cls, input_file_path: str, file_format: Optional[str] = None):
         if file_format is None:
             try:
-                file_format = file_path.split(".")[-1].rstrip()
+                file_format = input_file_path.split(".")[-1].rstrip()
             except IndexError:
                 print("Fail to get file format")
                 return
@@ -34,7 +40,7 @@ class Parser(ABC):
         for subclass in cls.__subclasses__():
             if subclass.__dict__.get("file_format") == file_format:
                 try:
-                    subclass.parse(file_path)
+                    subclass.parse(input_file_path)
                 except ParserException as ex:
                     print("Failure:", ex)
                 return
--- pars_script/concrete_parsers/yaml_parser.py
+++ pars_script/concrete_parsers/yaml_parser.py
@@ -1,8 +1,17 @@
+import datetime
 import json
-from typing import Dict, List
+import re
+from typing import Any, Dict
 import yaml
 from pars_script import Parser
 from pars_script.exceptions import ParserException
+
+
+class CustomEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (datetime.date, datetime.datetime)):
+            return obj.isoformat()
+        return json.JSONEncoder.default(self, obj)
 
 
 class YamlParser(Parser):
@@ -10,14 +19,22 @@ class YamlParser(Parser):
     file_format = "yaml"
 
     @staticmethod
-    def read_file(file_path: str):
-        with open(file_path, "r") as file:
+    def read_file(input_file_path: str):
+        with open(input_file_path, "r") as file:
             return yaml.load(file, Loader=yaml.CSafeLoader)
 
+    @staticmethod
+    def write_file(input_file_path: str, data: Any):
+        with open(input_file_path, "w") as file:
+            yaml.dump(data=data,
+                      stream=file,
+                      Dumper=yaml.CSafeDumper,
+                      allow_unicode=True)
+
     @classmethod
-    def parse(cls, file_path: str):
+    def parse(cls, input_file_path: str):
         print("run yaml parser")
-        yaml_obj: dict = cls.read_file(file_path=file_path)
+        yaml_obj: dict = cls.read_file(input_file_path=input_file_path)
         paths: dict = yaml_obj.get("paths")
         if paths is None:
             raise ParserException("Fail to get 'paths' from file")
@@ -36,12 +53,38 @@ class YamlParser(Parser):
         }
         
         requests: Dict[str, dict]
-        for requests in paths.values():
+        for path, requests in paths.items():
             if requests.get("components") is None:
                 continue
             for subsection_name in components.keys():
                 components[subsection_name].update(requests["components"].get(subsection_name, {}))
-            requests.pop("components")
-        yaml_obj["components"] = {k:v for k, v in components.items() if v != {}}
-        print(json.dumps(yaml_obj, indent=4))
-        
+            paths[path].pop("components")
+        paths = cls.__update_references(paths)
+        components = {k:v for k, v in components.items() if v != {}}
+        yaml_obj["components"] = components
+        cls.write_file(input_file_path="dump_file.yaml", data=yaml_obj)
+        with open("json_dump_file.json", "w") as file:
+            json.dump(yaml_obj, file, indent=1, cls=CustomEncoder)
+
+    @classmethod
+    def __update_references(cls, paths: dict) -> dict:
+        def __update_ref(value):
+            if isinstance(value, str) and name == "$ref":
+                components_index = value.find("components")
+                if components_index == -1:
+                    return
+                value = f"#/{value[components_index:]}"
+                paths[name] = value
+
+        for name, value in paths.items():
+            if isinstance(value, dict):
+                cls.__update_references(value)
+            if isinstance(value, list):
+                for vid in range(len(value)):
+                    if isinstance(value[vid], dict):
+                        value[vid] = cls.__update_references(value[vid])
+                    else:
+                        __update_ref(value)
+            else:
+                __update_ref(value)           
+        return paths
